717bb8266c2203b32834fb3f797acca263ba3daf
keshavpro1 <keshavprogrammer1@gmail.com>
2023-09-23 13:17:24 +0530

    init commit

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-const express = require("express");
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 
-const app = express();
+const api = express();
 
-app.use(express.json());
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
 
-app.get("/", async (req, res) => {
-  return res.json({ message: "Hello, World ✌️" });
-});
+api.use('/api/', router);
 
-const start = async () => {
-  try {
-    app.listen(3000, () => console.log("Server started on port 3000"));
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-start();
+export const handler = serverless(api);
